e08184fc0ea7d3b62dbda99e9870a10827bd7972
HyunsooPark <qkrgustnrk@gmail.com>
2017-08-21 08:49:06 +0900

    Updated MainInfoActivity

--- app/src/main/java/hpark/instagramfollowers_prototype/activity/MainInfoActivity.java
+++ app/src/main/java/hpark/instagramfollowers_prototype/activity/MainInfoActivity.java
@@ -3,15 +3,21 @@ package hpark.instagramfollowers_prototype.activity;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import butterknife.BindView;
 import hpark.instagramfollowers_prototype.R;
 import hpark.instagramfollowers_prototype.adapter.ShareGroupAdapter;
 import hpark.instagramfollowers_prototype.model.ShareGroupItem;
+import hpark.instagramfollowers_prototype.util.Constants;
 import hpark.instagramfollowers_prototype.util.DatabaseManager;
 
 /**
@@ -20,9 +26,22 @@ import hpark.instagramfollowers_prototype.util.DatabaseManager;
 
 public class MainInfoActivity extends AppCompatActivity {
 
+    @BindView(R.id.userImageView)
+    ImageView userImageView;
+    @BindView(R.id.userNameTextView)
+    TextView userNameTextView;
+    @BindView(R.id.userIntroTextView)
+    TextView userIntroTextView;
+    @BindView(R.id.numPostsTextView)
+    TextView numPostsTextView;
+    @BindView(R.id.numFollowingTextView)
+    TextView numFollowingTextView;
+    @BindView(R.id.numFollowersTextView)
+    TextView numFollowersTextView;
     @BindView(R.id.shareGroupListView)
     ListView shareGroupListView;
 
+    private HashMap<String, String> ownerInfo;
     DatabaseManager databaseManager;
     ArrayList<ShareGroupItem> shareGroupItems = new ArrayList<>();
     ShareGroupAdapter shareGroupAdapter;
@@ -35,7 +54,23 @@ public class MainInfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_info);
 
+        userNameTextView.setText(ownerInfo.get(Constants.TAG_USERNAME));
+        userIntroTextView.setText(ownerInfo.get(Constants.TAG_BIO));
+        numPostsTextView.setText(ownerInfo.get(Constants.TAG_MEDIA));
+        numFollowingTextView.setText(ownerInfo.get(Constants.TAG_FOLLOWS));
+        numFollowersTextView.setText(ownerInfo.get(Constants.TAG_FOLLOWED_BY));
+
         databaseManager = new DatabaseManager(this);
+        Picasso.with(this).load(ownerInfo.get(Constants.TAG_PROFILE_PICTURE)).into(userImageView);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
+
+
     }
 
     private void setShareGroupAdapter() {
@@ -60,6 +95,8 @@ public class MainInfoActivity extends AppCompatActivity {
         shareGroupListView = (ListView) findViewById(R.id.shareGroupListView);
         shareGroupListView.setAdapter(shareGroupAdapter);
     }
+
+
 
 
 
